723736df62c7a86890e76a846bed3558daab0ea4
DaviCastelo <davicastelo230800@gmail.com>
2025-09-24 09:45:16 -0300

    Adcional da tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,6 +110,19 @@ export default function RootLayout({
           }}
         />
         
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17552993084"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17552993084');
+            `
+          }}
+        />
+        
         {/* Structured Data */}
         <script
           type="application/ld+json"
